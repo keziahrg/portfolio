2a2f3455e51ac0b3ec45863cc2f50ef524852534
keziahrg <rg.keziah@gmail.com>
2022-10-09 10:43:11 +1300

    feat: button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,45 @@
+import styles from './Button.module.css'
+
+interface Props {
+    label: string
+    onClick?: () => void
+    type?: 'button' | 'submit'
+    href?: string
+    openInNewWindow?: boolean
+    disabled?: boolean
+}
+
+export const Button = ({
+    onClick,
+    href,
+    type = 'button',
+    openInNewWindow,
+    label,
+    disabled,
+}: Props) => {
+    return (
+        <>
+            {href ? (
+                <a
+                    className={styles.button}
+                    title={label}
+                    href={href}
+                    target={openInNewWindow ? '__blank' : '__self'}
+                    rel="noopener noreferrer"
+                >
+                    {label}
+                </a>
+            ) : (
+                <button
+                    className={styles.button}
+                    aria-label={label}
+                    onClick={onClick}
+                    type={type}
+                    disabled={disabled}
+                >
+                    {label}
+                </button>
+            )}
+        </>
+    )
+}
